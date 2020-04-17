40f1f6d5ea27a089b3021b13d411dc3be659ade2
Kacper Smal <kacpersmal@gmail.com>
2020-04-17 23:10:27 +0200

    Created Data Models for API

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public String value { get; set; }
+        [Required]
+        public ToDoFlags flag { get; set; }
+
+    }
+}
---
+++ ToDoApp/Models/ToDoLIst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDoLIst
+    {
+
+        public int Id { get; set; }
+        public String Label { get; set; }
+        public List<ToDoItem> items { get; set; }
+
+    }
+}
