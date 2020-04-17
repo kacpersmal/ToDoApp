a22a16dbe003676721edad1ccd82082de42c117d
Kacper Smal <kacpersmal@gmail.com>
2020-04-18 01:14:59 +0200

    Created Basic display of lists

--- ToDoApp/ClientApp/src/App.js
+++ ToDoApp/ClientApp/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-
-import './custom.css'
+import ToDoListContainer from './components/ToDoListContainer';
+import axios from 'axios';
+import ToDoList from './components/ToDoList';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-        <div>
-            Hello!
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { ToDoData : [], isLoading : true };
+        this.fetchListst = this.fetchListst.bind(this);
+    }
+
+    fetchListst() {
+        axios.get('ToDo/ListAll').then((response) => {
+            this.setState({ ToDoData: response.data[0], isLoading: false });
+        })
+    }
+
+    componentDidMount() {
+        this.fetchListst();
+    }
+
+    renderToDoLists() {
+        return (
+                <ToDoListContainer id="list-container" data={this.state.ToDoData} />
+            );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ? (<div>Im loading data</div>) :  this.renderToDoLists() }
+            </div>
+        );
+    }
 }
---
+++ ToDoApp/ClientApp/src/components/ToDoList.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from 'react';
+import { Card, CardText, CardBody,CardTitle} from 'reactstrap';
+class ToDoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <Card className="w-25">
+                    <CardBody>
+                        <CardTitle> {this.props.data.label} </CardTitle>
+                            <ol>
+                            {this.props.data.items.map((item, i) => (<li key={i}>{item.value}</li>))}
+                            </ol>
+                    </CardBody>
+                </Card>
+        );
+    }
+}
+
+export default ToDoList;
